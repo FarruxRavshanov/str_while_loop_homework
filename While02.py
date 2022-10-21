9e196ad3e84c87442b08b1b8a90ba02b0ad9d6a3
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-21 19:16:54 +0300

    test02

--- While02.py
+++ While02.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    a = 0
+    while i < len(s):
+
+        if s[i].isalpha():
+            a += 1
+
+        i += 1
+    return a
